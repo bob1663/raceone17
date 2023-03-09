b79078502e796d973fc6c3f2980553470e34472a
Vitaliy Mitnichuk <mitvitchuk@gmail.com>
2023-03-10 00:21:03 +0200

    fourth commit

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
-  )
-}
+    <footer className="app__footer">
+      <div className="app__footer-container">
+        <div className="app__footer-links">
+          <div className="app__footer-links_pages">
+            <p className="app__footer-links_pages-item">Terms and conditions</p>
+            <Link to="/contact" className="app__footer-links_pages-item">
+              Contact
+            </Link>
+            <Link to="/about" className="app__footer-links_pages-item">
+              About
+            </Link>
+          </div>
+          <div className="app__footer-links_social">
+            <p className="app__footer-links_pages-item">Sellfy</p>
+            <p className="app__footer-links_pages-item">Instagram</p>
+            <p className="app__footer-links_pages-item">YouTube</p>
+            <p className="app__footer-links_pages-item">Discord</p>
+            <p className="app__footer-links_pages-item">Twitter</p>
+            <p className="app__footer-links_pages-item">Facebook</p>
+          </div>
+        </div>
+        <div className="app__footer-rights">
+          <p>© 2023 Race Sim Studio. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
