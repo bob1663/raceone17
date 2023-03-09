7b10f6bca1ef414d5c6c707202fa897796cdf6c4
Vitaliy Mitnichuk <mitvitchuk@gmail.com>
2023-03-09 21:03:33 +0200

    third commit

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,59 +1,131 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
-import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import { RiCloseLine } from "react-icons/ri";
+import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
+  // ------------------------------------------------------
+  const [activeLink, setActiveLink] = useState(getActiveLinkFromStorage());
+  function getActiveLinkFromStorage() {
+    const storedLink = localStorage.getItem("activeLink");
+    return storedLink ? storedLink : "/";
+  }
+  // ------------------------------------------------------
+  const location = useLocation();
+  const isShopPage = location.pathname === "/shop";
+  const isNewsPage = location.pathname === "/news";
+  // ------------------------------------------------------
   return (
-    <nav className="app__navbar">
-      <div className="app__navbar-logo">
-        <h1>race</h1>
-        <h1>one</h1>
-        <h1>17</h1>
+    <nav className={`app__navbar ${isShopPage || isNewsPage ? "alt" : ""}`}>
+      <div className="app__navbar-container">
+        <Link
+          to="/"
+          className="app__navbar-logo"
+          onClick={() => {
+            setActiveLink("/");
+            localStorage.setItem("activeLink", "/");
+          }}
+        >
+          <h1>race</h1>
+          <h1>one</h1>
+          <h1>17</h1>
+        </Link>
+        <div className={`app__navbar-links ${isShopPage || isNewsPage ? "alt" : ""}`}>
+          <ul>
+            <li>
+              <Link
+                to="/"
+                className={`app__navbar-links_item ${
+                  activeLink === "/" ? "active" : ""
+                }`}
+                onClick={() => {
+                  setActiveLink("/");
+                  localStorage.setItem("activeLink", "/");
+                }}
+              >
+                home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/shop"
+                className={`app__navbar-links_item ${
+                  activeLink === "/shop" ? "active" : ""
+                }`}
+                onClick={() => {
+                  setActiveLink("/shop");
+                  localStorage.setItem("activeLink", "/shop");
+                }}
+              >
+                shop
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/news"
+                className={`app__navbar-links_item ${
+                  activeLink === "/news" ? "active" : ""
+                }`}
+                onClick={() => {
+                  setActiveLink("/news");
+                  localStorage.setItem("activeLink", "/news");
+                }}
+              >
+                news
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/contact"
+                className={`app__navbar-links_item ${
+                  activeLink === "/contact" ? "active" : ""
+                }`}
+                onClick={() => {
+                  setActiveLink("/contact");
+                  localStorage.setItem("activeLink", "/contact");
+                }}
+              >
+                contact
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/cars"
+                className={`app__navbar-links_item ${
+                  activeLink === "/cars" ? "active" : ""
+                }`}
+                onClick={() => {
+                  setActiveLink("/cars");
+                  localStorage.setItem("activeLink", "/cars");
+                }}
+              >
+                cars
+              </Link>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/the_jfdl/"
+                target="_blank"
+                className="app__navbar-links_item"
+              >
+                by jfdl
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="app__navbar-links">
-        <ul>
-          <li>
-            <Link to="/" className="app__navbar-links_item">
-              home
-            </Link>
-          </li>
-          <li>
-            <Link to="/shop" className="app__navbar-links_item">
-              shop
-            </Link>
-          </li>
-          <li>
-            <Link to="/news" className="app__navbar-links_item">
-              news
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact" className="app__navbar-links_item">
-              contact
-            </Link>
-          </li>
-          <li>
-            <Link to="/cars" className="app__navbar-links_item">
-              cars
-            </Link>
-          </li>
-        </ul>
-      </div>
+
       <div className="app__navbar-smallscreen">
         {toggleMenu ? (
           <RiCloseLine
-            color="#fff"
-            size={27}
+            color="#000000"
+            size={32}
             onClick={() => setToggleMenu(false)}
           />
         ) : (
-          <RiMenu3Line
-            color="#fff"
-            size={27}
-            onClick={() => setToggleMenu(true)}
-          />
+          <FiMenu color="#000000" size={27} onClick={() => setToggleMenu(true)} />
         )}
         {toggleMenu && (
           <div className="app__navbar-smallscreen_links">
@@ -74,7 +146,10 @@ const Navbar = () => {
                 </Link>
               </li>
               <li>
-                <Link to="/contact" className="app__navbar-smallscreen_links-item">
+                <Link
+                  to="/contact"
+                  className="app__navbar-smallscreen_links-item"
+                >
                   contact
                 </Link>
               </li>
@@ -82,6 +157,15 @@ const Navbar = () => {
                 <Link to="/cars" className="app__navbar-smallscreen_links-item">
                   cars
                 </Link>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/the_jfdl/"
+                  target="_blank"
+                  className="app__navbar-smallscreen_links-item"
+                >
+                  by jfdl
+                </a>
               </li>
             </ul>
           </div>
