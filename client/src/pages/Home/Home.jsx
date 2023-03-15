0db3054a1a6f7798b8df5681b57bab7a3c32b571
Vitaliy Mitnichuk <mitvitchuk@gmail.com>
2023-03-15 17:49:57 +0200

    seventh commit

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -5,10 +5,19 @@ import PrevButton from "../../assets/PrevButton.png";
 import { MainCarousel } from "../../components";
 
 const Home = () => {
-  return <div className="app__home">
+  return (
+    <div className="app__home">
+      <div className="app__carousel-buttons">
+        <div className="app__carousel-buttons-content">
+          <img src={PrevButton} alt="Previous" className="app__prev-button" />
+        </div>
+        <div className="app__carousel-buttons-content">
+          <img src={NextButton} alt="Next" className="app__next-button" />
+        </div>
+      </div>
       <MainCarousel />
-      
-  </div>;
+    </div>
+  );
 };
 
 export default Home;
