6a41f9b6ea958f4ae5d102e38346e7ae72551b6a
Vitaliy Mitnichuk <mitvitchuk@gmail.com>
2023-03-29 20:19:53 +0300

    28th commit

--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -4,52 +4,52 @@ import MCBg3 from "../assets/MainCarouselBg3.png";
 import MCBg4 from "../assets/MainCarouselBg4.png";
 import MCBg5 from "../assets/MainCarouselBg5.png";
 
-import ShopCard1 from "../assets/ShopCard1.png";
-import ShopCard2 from "../assets/ShopCard2.png";
-import ShopCard3 from "../assets/ShopCard3.png";
-import ShopCard4 from "../assets/ShopCard4.png";
-import ShopCard5 from "../assets/ShopCard5.png";
-import ShopCard6 from "../assets/ShopCard6.png";
-import ShopCard7 from "../assets/ShopCard7.png";
-import ShopCard8 from "../assets/ShopCard8.png";
-import ShopCard9 from "../assets/ShopCard9.png";
-import ShopCard10 from "../assets/ShopCard10.png";
-import ShopCard11 from "../assets/ShopCard11.png";
-import ShopCard12 from "../assets/ShopCard12.png";
-import ShopCard13 from "../assets/ShopCard13.png";
-import ShopCard14 from "../assets/ShopCard14.png";
-import ShopCard15 from "../assets/ShopCard15.png";
-import ShopCard16 from "../assets/ShopCard16.png";
-import ShopCard17 from "../assets/ShopCard17.png";
-import ShopCard18 from "../assets/ShopCard18.png";
-import ShopCard19 from "../assets/ShopCard19.png";
-import ShopCard20 from "../assets/ShopCard20.png";
-import ShopCard21 from "../assets/ShopCard21.png";
-import ShopCard22 from "../assets/ShopCard22.png";
-import ShopCard23 from "../assets/ShopCard23.png";
-import ShopCard24 from "../assets/ShopCard24.png";
+import ShopCard1 from "../assets/shop/ShopCard1.png";
+import ShopCard2 from "../assets/shop/ShopCard2.png";
+import ShopCard3 from "../assets/shop/ShopCard3.png";
+import ShopCard4 from "../assets/shop/ShopCard4.png";
+import ShopCard5 from "../assets/shop/ShopCard5.png";
+import ShopCard6 from "../assets/shop/ShopCard6.png";
+import ShopCard7 from "../assets/shop/ShopCard7.png";
+import ShopCard8 from "../assets/shop/ShopCard8.png";
+import ShopCard9 from "../assets/shop/ShopCard9.png";
+import ShopCard10 from "../assets/shop/ShopCard10.png";
+import ShopCard11 from "../assets/shop/ShopCard11.png";
+import ShopCard12 from "../assets/shop/ShopCard12.png";
+import ShopCard13 from "../assets/shop/ShopCard13.png";
+import ShopCard14 from "../assets/shop/ShopCard14.png";
+import ShopCard15 from "../assets/shop/ShopCard15.png";
+import ShopCard16 from "../assets/shop/ShopCard16.png";
+import ShopCard17 from "../assets/shop/ShopCard17.png";
+import ShopCard18 from "../assets/shop/ShopCard18.png";
+import ShopCard19 from "../assets/shop/ShopCard19.png";
+import ShopCard20 from "../assets/shop/ShopCard20.png";
+import ShopCard21 from "../assets/shop/ShopCard21.png";
+import ShopCard22 from "../assets/shop/ShopCard22.png";
+import ShopCard23 from "../assets/shop/ShopCard23.png";
+import ShopCard24 from "../assets/shop/ShopCard24.png";
 
-import NewsCard1 from "../assets/NewsCard1.png";
-import NewsCard2 from "../assets/NewsCard2.png";
-import NewsCard3 from "../assets/NewsCard3.png";
-import NewsCard4 from "../assets/NewsCard4.png";
-import NewsCard5 from "../assets/NewsCard5.png";
-import NewsCard6 from "../assets/NewsCard6.jpg";
-import NewsCard7 from "../assets/NewsCard7.jpg";
-import NewsCard8 from "../assets/NewsCard8.jpg";
-import NewsCard9 from "../assets/NewsCard9.jpg";
-import NewsCard10 from "../assets/NewsCard10.jpg";
-import NewsCard11 from "../assets/NewsCard11.jpg";
-import NewsCard12 from "../assets/NewsCard12.jpg";
-import NewsCard13 from "../assets/NewsCard13.jpg";
-import NewsCard14 from "../assets/NewsCard14.jpg";
-import NewsCard15 from "../assets/NewsCard15.jpg";
-import NewsCard16 from "../assets/NewsCard16.jpg";
-import NewsCard17 from "../assets/NewsCard17.png";
-import NewsCard18 from "../assets/NewsCard18.png";
-import NewsCard19 from "../assets/NewsCard19.jpg";
-import NewsCard20 from "../assets/NewsCard20.jpg";
-import NewsCard21 from "../assets/NewsCard21.jpg";
+import NewsCard1 from "../assets/news/NewsCard1.png";
+import NewsCard2 from "../assets/news/NewsCard2.png";
+import NewsCard3 from "../assets/news/NewsCard3.png";
+import NewsCard4 from "../assets/news/NewsCard4.png";
+import NewsCard5 from "../assets/news/NewsCard5.png";
+import NewsCard6 from "../assets/news/NewsCard6.jpg";
+import NewsCard7 from "../assets/news/NewsCard7.jpg";
+import NewsCard8 from "../assets/news/NewsCard8.jpg";
+import NewsCard9 from "../assets/news/NewsCard9.jpg";
+import NewsCard10 from "../assets/news/NewsCard10.jpg";
+import NewsCard11 from "../assets/news/NewsCard11.jpg";
+import NewsCard12 from "../assets/news/NewsCard12.jpg";
+import NewsCard13 from "../assets/news/NewsCard13.jpg";
+import NewsCard14 from "../assets/news/NewsCard14.jpg";
+import NewsCard15 from "../assets/news/NewsCard15.jpg";
+import NewsCard16 from "../assets/news/NewsCard16.jpg";
+import NewsCard17 from "../assets/news/NewsCard17.png";
+import NewsCard18 from "../assets/news/NewsCard18.png";
+import NewsCard19 from "../assets/news/NewsCard19.jpg";
+import NewsCard20 from "../assets/news/NewsCard20.jpg";
+import NewsCard21 from "../assets/news/NewsCard21.jpg";
 
 import ShareIcon from "../assets/ShareIcon2.svg";
 import ReadMoreIcon from "../assets/ReadMoreIcon2.svg";
@@ -71,6 +71,13 @@ import f12020 from "../assets/formula/rss-hybrid_2020-2.jpg";
 import f12021 from "../assets/formula/fh21_1-scaled.jpg";
 import f12022 from "../assets/formula/fh22_website.jpg";
 import f12022x from "../assets/formula/formulax.jpeg";
+import f12017temp from "../assets/formula/f12017temp.jpeg";
+import f12018temp from "../assets/formula/f12018temp.jpeg";
+import f12019temp from "../assets/formula/f12019temp.jpeg";
+import f12020temp from "../assets/formula/f12020temp.jpeg";
+import f12021temp from "../assets/formula/f12021temp.jpeg";
+import f12022temp from "../assets/formula/f12022temp.jpg";
+import f12022xtemp from "../assets/formula/formulaxtemp.jpeg";
 
 import bmwm6gt3 from "../assets/gt/bmwm6gt3.jpg";
 import ferrari360gt3 from "../assets/gt/fer36.jpg";
@@ -82,16 +89,37 @@ import porsche911gt3 from "../assets/gt/porsche911.jpg";
 import saleen from "../assets/gt/shadow_main.jpg";
 import tornado from "../assets/gt/tornado_preview2.jpg";
 import vipergt3 from "../assets/gt/vortex_preview.jpg";
+import bmwm6gt3temp from "../assets/gt/m6gt3temp.jpeg";
+import ferrari360gt3temp from "../assets/gt/36temp.jpeg";
+import ferrari575gt3temp from "../assets/gt/57temp.jpeg";
+import ferrari550gt3temp from "../assets/gt/55temp.jpeg";
+import huracangt3temp from "../assets/gt/huracangt3temp.jpeg";
+import murcielagotemp from "../assets/gt/lanzotemp.jpeg";
+import porsche911gt3temp from "../assets/gt/911temp.jpeg";
+import saleentemp from "../assets/gt/shadowtemp.jpeg";
+import tornadotemp from "../assets/gt/tornadotemp.jpeg";
+import vipergt3temp from "../assets/gt/vortextemp.jpeg";
 
 import lock from "../assets/lock.svg";
 import user from "../assets/user.svg";
 import loginIco from "../assets/loginIco.svg";
 
+import CarsIconArrow from "../assets/CarsIconArrow.svg";
+
 export default {
+  CarsIconArrow,
+
   lock,
   user,
   loginIco,
 
+  f12017temp,
+  f12018temp,
+  f12019temp,
+  f12020temp,
+  f12021temp,
+  f12022temp,
+  f12022xtemp,
   f12017,
   f12018,
   f12019,
@@ -99,6 +127,17 @@ export default {
   f12021,
   f12022,
   f12022x,
+
+  bmwm6gt3temp,
+  ferrari360gt3temp,
+  ferrari575gt3temp,
+  ferrari550gt3temp,
+  huracangt3temp,
+  murcielagotemp,
+  porsche911gt3temp,
+  saleentemp,
+  tornadotemp,
+  vipergt3temp,
   bmwm6gt3,
   ferrari360gt3,
   ferrari575gt3,
--- client/src/pages/Cars/Cars.jsx
+++ client/src/pages/Cars/Cars.jsx
@@ -1,10 +1,112 @@
-import React from 'react';
-import './Cars.css';
+import React, { useRef } from "react";
+import "./Cars.css";
+
+import { images, data } from "../../constants";
 
 const Cars = () => {
-  return (
-    <div>Cars</div>
-  )
-}
+  // -------------------------------------------------------------------------
+  const gtCarElements = data.gtCars.map((gtCar) => {
+    return (
+      <div className="app__cars-bottomDiv_content-item" key={gtCar.id}>
+        <p>In stock</p>
+        <h3>{gtCar.name}</h3>
+        <h3>{gtCar.power}</h3>
+        <h3>{gtCar.weight}</h3>
+        <h3>March 25, 2023</h3>
+      </div>
+    );
+  });
 
-export default Cars
+  const f1CarElements = data.openWheel.map((openW) => {
+    return (
+      <div className="app__cars-bottomDiv_content-item" key={openW.id}>
+        <p>In stock</p>
+        <h3>{openW.name}</h3>
+        <h3>{openW.power}</h3>
+        <h3>{openW.weight}</h3>
+        <h3>March 25, 2023</h3>
+      </div>
+    );
+  });
+  // -------------------------------------------------------------------------
+  const openWheelRef = useRef(null); // add a ref for the open-wheel h1 element
+  const gtRef = useRef(null); // add a ref for the gt h1 element
+  // add a function to handle scrolling to the open-wheel h1 element
+  const handleOpenWheelClick = () => {
+    openWheelRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  // add a function to handle scrolling to the gt h1 element
+  const handleGtClick = () => {
+    gtRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+  // -------------------------------------------------------------------------
+
+  return (
+    <div className="app__cars">
+      <div className="app__cars_topDiv">
+        <div
+          className="app__cars_topDiv-openWheel"
+          onClick={handleOpenWheelClick}
+        >
+          <h1>open-wheel cars</h1>
+        </div>
+        <div className="app__cars_topDiv-GT" onClick={handleGtClick}>
+          <h1>gt cars</h1>
+        </div>
+      </div>
+      <div className="app__cars-bottomDiv">
+        <div className="app__cars-bottomDiv_content" ref={openWheelRef}>
+          <h1>open-wheel car list</h1>
+          <span></span>
+          <div className="app__cars-bottomDiv_content-categories">
+            <h3>
+              Status <img src={images.CarsIconArrow} alt="CarsIconArrow" />
+            </h3>
+            <h3>
+              Name <img src={images.CarsIconArrow} alt="CarsIconArrow" />
+            </h3>
+            <h3>
+              Power <img src={images.CarsIconArrow} alt="CarsIconArrow" />
+            </h3>
+            <h3>
+              Weight <img src={images.CarsIconArrow} alt="CarsIconArrow" />
+            </h3>
+            <h3>
+              Created <img src={images.CarsIconArrow} alt="CarsIconArrow" />
+            </h3>
+          </div>
+          {f1CarElements}
+        </div>
+        <div
+          className="app__cars-bottomDiv_content"
+          style={{ paddingBottom: "100px" }}
+          ref={gtRef}
+        >
+          <h1>gt car list</h1>
+          <span></span>
+          <div className="app__cars-bottomDiv_content-categories">
+            <h3>
+              Status <img src={images.CarsIconArrow} alt="CarsIconArrow" />
+            </h3>
+            <h3>
+              Name <img src={images.CarsIconArrow} alt="CarsIconArrow" />
+            </h3>
+            <h3>
+              Power <img src={images.CarsIconArrow} alt="CarsIconArrow" />
+            </h3>
+            <h3>
+              Weight <img src={images.CarsIconArrow} alt="CarsIconArrow" />
+            </h3>
+            <h3>
+              Created <img src={images.CarsIconArrow} alt="CarsIconArrow" />
+            </h3>
+          </div>
+          {gtCarElements}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cars;
